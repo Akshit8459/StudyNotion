066b5d79a8b182ea793a2d08420789c9b073ebf6
Akshit8459 <akshitpokhriyal@gmail.com>
2023-11-27 06:33:44 +0530

    starting frontend dev now

---
+++ studynotion/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-red-600 text-3xl">STUDYNOTION</h1>
+    </div>
+  );
+}
+
+export default App;
